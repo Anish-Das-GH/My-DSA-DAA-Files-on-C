94c2f27a0eab3f4e20cf88dd23c500fafad8dffd
Anish Das <anishservice12@gmail.com>
2022-10-22 22:09:41 +0530

    Second Commit

--- test.c
+++ test.c
@@ -1,70 +1,66 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void arraytraverse(int n,int *A)
+void printarray(int *A,int n)
 {
     int i;
     for ( i = 0; i < n; i++)
     {
         printf(" %d ",A[i]);
+
     }
     printf("\n");
+    
 }
 
 int Partition(int A[],int low,int high)
 {
     int pivot = A[low];
     int i = low+1;
-    int j  =high;
+    int j = high;
     int temp;
+
     do
     {
         while (A[i]<=pivot)
         {
             i++;
         }
+
         while (A[j]>pivot)
         {
             j--;
         }
+        
         if (i<j)
         {
-            temp = A[i];
-            A[i] = A[j];
-            A[j] = temp;
+       temp = A[i];
+       A[i] = A[j];
+       A[j]=temp;
         }
-        
-        
         
     } while (i<j);
     temp = A[low];
-    A[low] = A[j];
+    A[low]= A[j];
     A[j] = temp;
     return j;
-    
 }
 
 void Quicksort(int A[],int low,int high)
 {
-    int partitionindex;
-    if (low<high)
+    int Partitionindex;
+    if(low<high)
     {
-        partitionindex = Partition(A,low,high);
-        Quicksort(A,low,partitionindex-1);
-        Quicksort(A,partitionindex+1,high);
+        Partitionindex = Partition(A,low,high);
+        Quicksort(A,low,Partitionindex-1);
+        Quicksort(A,Partitionindex+1,high);
     }
-    
 }
 
 int main()
 {
+    int A[]={17,3,4,2,10,43,32,23,56};
+    int n=9;
+    Quicksort(A,0,n-1);
+    printarray(A,n);
 
-int A[]={12,5,7,8,9,3,98,6};
-int n = 8;
-Quicksort(A,0,n-1);
-arraytraverse(n,A);
-
-
-
-
-    return 0;
 }
